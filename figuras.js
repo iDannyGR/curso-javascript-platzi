06f800ecc750e6ef7e7ba9599693935c081a1b00
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2021-09-28 08:47:52 -0600

    new file:   figuras.html new file:   figuras.js

---
+++ figuras.js
@@ -0,0 +1,10 @@
+'use strict'
+
+<<<<<<< HEAD
+window.addEventListener ('load', () =>{
+    console.log('prueba de carga de archivo js')
+=======
+window.addEventListener('load', () =>{
+    
+>>>>>>> ab91da41910fbb960b03a04dfb70a1df5625f7f5
+})
