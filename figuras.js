4619c7ecf5e3308916c9a526b5c1c924bf07b768
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2021-09-28 11:28:54 -0600

    version pc de alcaldia

--- figuras.js
+++ figuras.js
@@ -1,10 +1,35 @@
 'use strict'
+function perimetroCuadrado(lado){
+        let resultado = lado * 4;
+        return alert('el perimetro es: '+resultado +'cm');
+}
 
-<<<<<<< HEAD
+function areaCuadrado(lado){
+    let resultado = Math.pow(lado,2);
+    return alert('el area es: '+resultado + 'cm2');
+}
+
+
 window.addEventListener ('load', () =>{
-    console.log('prueba de carga de archivo js')
-=======
-window.addEventListener('load', () =>{
+    // AREA DE UN CUADRADO
+let usuario = prompt('ingrese la medida del lado del cuadrado:',0);
+        perimetroCuadrado(usuario);
+            //PERIMETRO DE UN CUADRADO
+        areaCuadrado(usuario);
     
->>>>>>> ab91da41910fbb960b03a04dfb70a1df5625f7f5
-})
+//triangulo
+//perimetro del triangulo
+let triangulo = {
+    lado1: 5,
+    lado2: 5,
+    base: 6.5,
+    altura: 4.2
+}
+
+let perimetroTriangulo = triangulo.lado1+triangulo.lado2+triangulo.base;
+console.log('el perimetro del triangulo es: '+perimetroTriangulo);
+ 
+//area de triangulo
+ let areaTriangulo = (triangulo.base*triangulo.altura)/2;
+ console.log('el area del triangulo es: '+areaTriangulo);
+});
