886e2a7b880cebe85d12fe8ccb49dfe3640da073
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2021-09-30 15:32:10 -0600

    pc alcaldia 30/09/2021 tarde

--- figuras.js
+++ figuras.js
@@ -15,6 +15,14 @@ function areaTriangulo(base, altura){
     return alert(`el area del triangulo es: ${(base*altura)/2}`)
 };
 
+function isoseles(lado1,lado2,base){
+    if(lado1===lado2 && lado1 != base){
+        return alert(`la altura del triangulo isoseles es: ${Math.sqrt(lado1**2 - base**2/ 4)}`)
+    }else{
+        return alert('no es isoseles')
+    }
+    
+};
 
 function circunferenciaCirculo (diametro){
     return alert('la circunferencia es:'+ diametro * Math.PI)
@@ -37,21 +45,47 @@ window.addEventListener ('load', () =>{
             let ladoCuadra = document.querySelector('#lado_cuadrado').value;
             perimetroCuadrado(ladoCuadra);
         });
-    
-
-
-    
     //triangulo
     //perimetro del triangulo
-    let  lado1 =5;
-    let lado2  =5;
-    let  base  =5;
-    let altura =5; 
-//area de triangulo
+let calPerimetrotriangulo= document.getElementById('perimetro_tri');
+        calPerimetrotriangulo.addEventListener('click',()=>{
+            let lado1 =document.querySelector('#lado1_triangulo').value;
+            let lado2 =document.querySelector('#lado2_triangulo').value;
+            let base =document.querySelector('#base_triangulo').value;
+            perimetroTriangulo(parseInt(lado1),parseInt(lado2),parseInt(base));
+        })
+
+    //area de triangulo
+        let calAreatriangulo= document.getElementById('area_tri');
+        calAreatriangulo.addEventListener('click',()=>{
+            let base =document.querySelector('#base_triangulo').value;
+            let altura =document.querySelector('#altura_triangulo').value;
+            areaTriangulo(parseInt(base), parseInt(altura));
+        });
+    //altura isoseles
+    let trisoseles= document.getElementById('isoseles_tri');
+        trisoseles.addEventListener('click',() =>{
+            let lado1 =document.querySelector('#lado1_triangulo').value;
+            let lado2 =document.querySelector('#lado2_triangulo').value;
+            let base =document.querySelector('#base_triangulo').value;
+             isoseles(parseInt(lado1),parseInt(lado2),parseInt(base));
+        });
+
  
 //circulo
-let radio=4;
-let diametro = radio * 2;
-
+        //area de circulo
+        let areaCir= document.getElementById('area_Circulo')
+        areaCir.addEventListener('click',()=>{
+            let radio = document.querySelector('#radio').value;
+            let diametro = radio * 2;
+                areaCirculo(radio);
+        });
+        //circunferencia/perimetro circulo
+        let perimetroCir= document.getElementById('perimetro_Circulo')
+        perimetroCir.addEventListener('click',()=>{
+            let radio = document.querySelector('#radio').value;
+            let diametro = radio * 2;
+                circunferenciaCirculo(diametro);
+        });
 
 });
