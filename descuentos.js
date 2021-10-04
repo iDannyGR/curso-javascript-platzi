a38f1f2fe78650dd0d32352ec47df44283199e11
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2021-10-04 15:59:22 -0600

    pc alcaldia version tarde

--- descuentos.js
+++ descuentos.js
@@ -9,16 +9,27 @@ function descuento (precio, porcentaje){
     //evento load
 window.addEventListener('load',()=>{
         let getDescuento = document.getElementById('aplDescuento');
-        
+        let cupon = [
+            {name:'verano_feliz',
+            des: 15},
+            {name:'invierno_congelado',
+            des: 25},
+            {name:'octubre_terror',
+            des: 5}
+        ];
+
+
     //evento click
     getDescuento.addEventListener('click', ()=>{
         let price = document.querySelector('#precio').value;
         let percen = document.querySelector('#porcentaje').value;
         let respuesta = document.getElementById('respuesta');  
-           
+        let extraDes= document.querySelector('#cupon').value;
+
         if(price == '' || percen == '' ){
             respuesta.innerHTML = 'Datos Erroneos';
         }else{
+            
         descuento(price,percen);
         }
 
