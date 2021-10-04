9cbf2912559308dbd573edf93fe95fe9733b47e3
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2021-10-03 20:12:22 -0600

    version pc casa 02/10/2021 noche

--- descuentos.js
+++ descuentos.js
@@ -1,11 +1,26 @@
 // funciones
 function descuento (precio, porcentaje){
-    let resultado = (porcentaje * precio) / 100;
-}
+    let respuesta = document.getElementById('respuesta');
+        let desApli = (porcentaje * precio) / 100;
+        let  valorFinal = precio - desApli;
+        respuesta.innerHTML = `el descuento es ${desApli},</br> y el valor final es ${valorFinal}`;
+};
 
-
-
+    //evento load
 window.addEventListener('load',()=>{
-    let precio = '';
-    let procentaje= '';
-})
+        let getDescuento = document.getElementById('aplDescuento');
+        
+    //evento click
+    getDescuento.addEventListener('click', ()=>{
+        let price = document.querySelector('#precio').value;
+        let percen = document.querySelector('#porcentaje').value;
+        let respuesta = document.getElementById('respuesta');  
+           
+        if(price == '' || percen == '' ){
+            respuesta.innerHTML = 'Datos Erroneos';
+        }else{
+        descuento(price,percen);
+        }
+
+    });
+});
