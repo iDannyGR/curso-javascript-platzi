3177d66353c07bfc80b7b1d1105b2597956872ba
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2021-10-07 20:05:45 -0600

    version pc casa 07/10/2021

--- promedio.js
+++ promedio.js
@@ -1,46 +1,40 @@
-//funciones
+//funciones 
 function mediaAritmetica (lista){
-let promedio = lista.reduce((x,y)=> x+y);    
-return (promedio/lista.length);
-};
-
-//load
-
-window.addEventListener('load', ()=>{
-    //promedio
-
-   
-});
-
-
-/*
-let suma = 0;
-let promedio = 0;
-for(let i = 0; i<lista.length; i++){
-    suma += lista[i];
-}
-promedio =  suma / lista.length;
-
-*/
-
-/*
-// media aritmetica
-
-variable tipo array en un pront o un input (-1 ya que los array comienzan en 0)
-solicitar la cantidad de datos
-solicitar los datos uno por uno
-realizar la sumatoria de cada uno de ellos con un for of o foreach en una variable despues
-dividir la variable entre array.lenth
-
-//mediana
-
-si es inpar el array la mediana es la mitad del array
-array.length / 2 = si es faccion se aproxima al siguiente y ese es la mitad
-si da un entero la mediana es el numero resultado y numero +1
-
-
-//moda
-
-el valor que mas se repite
-
-*/
+    let promedio = lista.reduce((x,y)=> x+y);    
+    return (promedio/lista.length);
+    };
+    
+    function espar(valor){
+        if(valor.length % 2 === 0){
+            return true
+        }else{return false}
+    };
+    //load
+    
+    window.addEventListener('load', ()=>{
+        //promedio
+    let arreglo = [500,200,100,500];
+    let mitad =parseInt(arreglo.length /2);
+    let mediana;
+        if(espar(arreglo.length)){
+            mediana = (arreglo[mitad] + arreglo[(mitad+1)])/2;
+            console.log(mediana);
+        }else{
+            mediana = arreglo[mitad];
+            console.log(mediana);
+        };
+    });
+    
+    
+    //mediana
+    /*
+    si es inpar el array la mediana es la mitad del array
+    array.length / 2 = si es faccion se aproxima al siguiente y ese es la mitad
+    si da un entero la mediana es el numero resultado y numero +1
+    
+    
+    //moda
+    
+    el valor que mas se repite
+    
+    */
