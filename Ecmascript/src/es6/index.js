cfaaab91519dc8f5abf904b24bb896a867cf4135
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2021-10-12 18:16:52 -0600

    viejito

--- Ecmascript/src/es6/index.js
+++ Ecmascript/src/es6/index.js
@@ -21,3 +21,21 @@ console.log(epic);
 //template literal
 let eppic2= `${hello} ${world}`;
 console.log(eppic2);
+
+let lorem = "quiero escribir una frase epica \n"
++ "otra frase eqpica que necesitamos.";
+//es6
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase eqpica que queremos`;
+
+console.log(lorem);
+console.log(lorem2);
+
+let person ={
+    'name':     'oscar',
+    'age':      32,
+    'country':  'ES'
+}
+console.log(person.name, person.age, person.country);
+let{name,age,country} =person;
+console.log(name,age,country);
