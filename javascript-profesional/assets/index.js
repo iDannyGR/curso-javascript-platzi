df38b0d9ef2da1d145862945d22cc0fd96df4f06
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2021-10-28 20:31:59 -0600

    casa 28/10/21

--- javascript-profesional/assets/index.js
+++ javascript-profesional/assets/index.js
@@ -1,12 +1,14 @@
 import MediaPlayer from './mediaPlayer.js'
 import AutoPlay from './plugins/autoPlay.js'
+import autoPause from './plugins/autoPause.js'
 
     const video = document.querySelector('video');
     const play = document.getElementById('play-pause');
     const mute = document.getElementById('mute-unmute');
  
-    const player = new MediaPlayer({ video, plugins: [ new AutoPlay(video) ] });
+    const player = new MediaPlayer({ video, plugins: [ new AutoPlay(video), new autoPause() ] });
     play.onclick=() => {player.tooglePlayPause()};
     mute.onclick=() => {player.toogleMuteUnmute()};
+
 
    
---
+++ javascript-profesional/assets/plugins/autoPause.js
@@ -0,0 +1,7 @@
+class autoPause{
+    constructor(video){
+
+    }
+}
+
+export default autoPause;
