9ee63b8c3f95bd3e869e4bd059a21d71c5fdaef1
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2021-11-01 15:33:07 -0600

    version alcaldia 1/11/21

--- javascript-profesional/assets/index.js
+++ javascript-profesional/assets/index.js
@@ -1,14 +1,18 @@
 import MediaPlayer from './mediaPlayer.js'
 import AutoPlay from './plugins/autoPlay.js'
+import AutoPause from './plugins/autoPause.js'
 
     const video = document.querySelector('video');
     const play = document.getElementById('play-pause');
     const mute = document.getElementById('mute-unmute');
  
-    const player = new MediaPlayer({ video, plugins: [ new AutoPlay()] });
+    const player = new MediaPlayer({ 
+        video, 
+        plugins:[new AutoPlay(), new AutoPause() ], 
+    });
+
     play.onclick=() => {player.tooglePlayPause()};
     mute.onclick=() => {player.toogleMuteUnmute()};
-
 
    if('serviceWorker' in navigator){
        navigator.serviceWorker.register('/sw.js').catch(error=>{
