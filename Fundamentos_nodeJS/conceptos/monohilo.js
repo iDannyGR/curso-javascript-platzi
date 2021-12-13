279f214fadca6c2a9286f31a05d3b726e97ac642
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2021-12-13 15:01:51 -0600

    [update]fundamentos alcaldia 2021

---
+++ Fundamentos_nodeJS/conceptos/monohilo.js
@@ -0,0 +1,12 @@
+console.log('hola mundo');
+let i = 0;
+
+setInterval(()=>{
+        console.log(i++);
+        // if(i===5){
+        //     var a = 3+z;
+        // }
+       console.log('sigo activo'); 
+}, 1000 );
+
+console.log('segunda instruccion');
