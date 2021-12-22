83b3422aaa4698e4b0d6133516c124b3f677523c
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2021-12-21 20:40:05 -0600

    [update] uso de trycath video a medias casa 21/12/21

---
+++ Fundamentos_nodeJS/modulos/console.js
@@ -0,0 +1,54 @@
+// console.log('algo');
+// console.error('algo con error');
+// console.warn('algo con alerta precausion');
+
+// let table = [
+//     {a:1,
+//     b:2
+//     },
+//     {
+//         a:2,
+//         b:'otro'
+//     },
+// ]
+
+// console.table(table);
+// console.group('conver')
+// console.log('hola');
+// console.group('conver')
+// console.log('hola');
+// console.log('bla bla bla');
+// console.log('adios');
+// console.groupEnd('conver')
+// console.log('bla bla bla');
+// console.log('adios');
+// console.groupEnd('conver')
+
+// function funcion1(){
+//     //
+//     console.group('funcion 1')
+//     console.log('esto es de la funcion 1');
+//     console.log('esto tambien');
+//     funcion2();
+//     console.log('hemos vuelto a la 1');
+//     console.groupEnd('funcion 1')
+// }
+// function funcion2(){
+//     //
+//     console.group('funcion 1')
+//     console.log('esto es de la funcion 2');
+//     console.log('esto tambien');
+//     console.groupEnd('funcion 1')
+// }
+
+// funcion1();
+
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.countReset('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
---
+++ Fundamentos_nodeJS/modulos/errores.js
@@ -0,0 +1,10 @@
+function serompe() { 
+       return 3+z;
+}
+ try {
+    serompe();
+ } catch (error) {
+    console.error('no es un valor numerico' + error);
+    console.error(error);
+ }
+ 
