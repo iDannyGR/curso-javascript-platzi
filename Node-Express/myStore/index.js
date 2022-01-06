95ba267b22820a3af6d60cb494b375c6075e8214
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-01-06 15:39:17 -0600

    [UPDATE] division y creacion de rutas, antes de instalar postman o insomnia alcaldia 6/1/22

--- Node-Express/myStore/index.js
+++ Node-Express/myStore/index.js
@@ -9,25 +9,11 @@ myApp.get('/', (req, res)=>{
       res.send('pagina principal');
 });
 
-myApp.get('/home', (req, res)=>{
-  res.send('home');
-});
-
 
 routerApi(myApp);
 
-myApp.get('/users', (req, res)=>{
-  const {limit, offset} =req.query;
-   limit && offset ? res.json({ limit,offset}) : res.send('no hay parametros');
-});
 
-myApp.get('/categories/:categoryId/products/:productId', (req, res)=>{
-    const {categoryId, productId} = req.params;
-    res.json({
-      categoryId,
-      productId
-    })
-});
+
 
 myApp.listen(port, ()=>{
   console.log('servidor corriendo');
