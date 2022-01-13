cdb4bd4c3da8c0d734eb7e55439d6102565e79e1
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-01-13 15:57:44 -0600

    [update] alcaldia con cors 13/1/22

--- Node-Express/myStore/index.js
+++ Node-Express/myStore/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routerApi = require("./routes");
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler');
+const cors = require('cors')
 const myApp = express();
 const port = 3000;
 
@@ -10,12 +11,22 @@ myApp.use(express.json())
 myApp.get('/', (req, res)=>{
       res.send('pagina principal');
 });
+const whiteList = ['http://localhost:5500/'];
+const coorsOption = {
+  origin: (origin, callback)=>{
+    (whiteList.includes(origin)) ?
+    callback(null, true):
+    callback(new Error('not Have a Persmission'))
+      }
+  }
 
 
+myApp.use(cors(coorsOption));
 routerApi(myApp);
 myApp.use(logErrors);
 myApp.use(boomErrorHandler);
 myApp.use(errorHandler);
+
 
 
 
