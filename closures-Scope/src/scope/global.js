9e2e8708b1d58d455ca5db2762d233d17d1a2086
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-02-16 20:40:50 -0600

    update 1 clase 2

---
+++ closures-Scope/src/scope/global.js
@@ -0,0 +1,12 @@
+var hello = 'HOLA';
+let world = 'hola mundo';
+const helloword = 'hellow world';
+
+const anotherfunction =()=>{
+    console.log(hello);
+    console.log(world);
+    console.log(helloword);
+}
+
+anotherfunction();
+
